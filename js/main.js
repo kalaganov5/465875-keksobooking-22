dd7cddffab4b4a3883a2bd98091d11b19afc8b25
Kalaganov5 <kalaganov5@gmail.com>
2021-02-07 15:11:28 +0300

    Правка после проверки настравником

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const getRandomNumber = function (numberMin, numberMax) {
 // Generate random whole number
 const getRandomWholeNumber = function (min, max) {
   if(min >= max || min < 0) {
-    throw new Error('getRandomWholeNumber — что пошло не так');
+    throw new Error('getRandomWholeNumber — Неправильный диапазон! Укажите любые числа от 0, чтобы первое было меньше второго.');
   }
   return Math.floor(getRandomNumber(min, max));
 }
@@ -22,10 +22,10 @@ const getRandomFractionalNumber = function (min, max, symbolsAfterPoint) {
 
   let randomNumber = (getRandomNumber(min, max)).toFixed(symbolsAfterPoint);
   // If the number is fractional and ends in 0
-  if(randomNumber.slice(-1) === '0' && ((randomNumber ^ 0) === randomNumber) === false) {
+  if(randomNumber.slice(-1) === '0' && randomNumber % 1 === 0) {
     randomNumber = randomNumber.substring(0, randomNumber.length - 1) + getRandomWholeNumber(1, 9) + randomNumber.substring(1 + randomNumber.length - 1);
   }
-  return Number(randomNumber);
+  return +randomNumber;
 }
 
 getRandomWholeNumber(0, 1);
