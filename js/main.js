09f90a9b0ea5ef73226c3996090e8e6336f0f4a8
Kalaganov5 <kalaganov5@gmail.com>
2021-02-12 10:45:44 +0300

    Задание больше деталей

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Формула взята на: https://learn.javascript.ru/task/random-int-min-max
 // Generate random number
 const getRandomNumber = function (numberMin, numberMax) {
-  return numberMin + Math.random() * (numberMax + 1 - numberMin);
+  return numberMin + Math.random() * (numberMax - numberMin);
 }
 
 // Generate random whole number
@@ -22,11 +22,87 @@ const getRandomFractionalNumber = function (min, max, symbolsAfterPoint) {
 
   let randomNumber = (getRandomNumber(min, max)).toFixed(symbolsAfterPoint);
   // If the number is fractional and ends in 0
-  if(randomNumber.slice(-1) === '0' && randomNumber % 1 === 0) {
+  if(randomNumber.slice(-1) === '0' && randomNumber % 1 !== 0) {
     randomNumber = randomNumber.substring(0, randomNumber.length - 1) + getRandomWholeNumber(1, 9) + randomNumber.substring(1 + randomNumber.length - 1);
   }
   return +randomNumber;
 }
 
-getRandomWholeNumber(0, 1);
-getRandomFractionalNumber(0, 2, 1);
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIONS = [
+  'What is Lorem Ipsum?',
+  'Why do we use it?',
+  'Where can I get some?',
+  'Where does it come from?',
+];
+
+const IMAGES = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const featuresUsed = [];
+for (let i = 0; i <= getRandomWholeNumber(0, FEATURES.length); i++) {
+  featuresUsed[i] = FEATURES[i];
+}
+
+const photosUsed = [];
+for (let i = 0; i <= getRandomWholeNumber(0, IMAGES.length); i++) {
+  photosUsed[i] = IMAGES[i];
+}
+
+const createDataKeksobooking = () => {
+  let author = {
+    avatar: `img/avatars/user0${getRandomWholeNumber(1, 8)}.png`,
+  };
+  let offer = {
+    title: DESCRIPTIONS[getRandomWholeNumber(0, DESCRIPTIONS.length - 1)],
+    address: `${getRandomFractionalNumber(35.65, 35.7, 5)}, ${getRandomFractionalNumber(139.7, 139.8, 5)}`,
+    price: getRandomWholeNumber(1000, 10000),
+    type: TYPES[getRandomWholeNumber(0, TYPES.length - 1)],
+    rooms: getRandomWholeNumber(1, 10),
+    guests: getRandomWholeNumber(1, 30),
+    checkin: TIMES[getRandomWholeNumber(0, TIMES.length - 1)],
+    checkout: TIMES[getRandomWholeNumber(0, TIMES.length - 1)],
+    features: featuresUsed,
+    description: DESCRIPTIONS[getRandomWholeNumber(0, DESCRIPTIONS.length - 1)],
+    photos: photosUsed,
+  };
+
+  let location = {
+    x: getRandomFractionalNumber(35.65, 35.7, 5),
+    y: getRandomFractionalNumber(139.7, 139.8, 5),
+  };
+
+  return {
+    author,
+    offer,
+    location,
+  }
+};
+
+const createArrayKeksobooking = new Array(10).fill(null).map(() => createDataKeksobooking());
+
+createArrayKeksobooking;
