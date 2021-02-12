5a4ddc3b0d300e459f40e5fb5fa3ba649dd60631
Kalaganov5 <kalaganov5@gmail.com>
2021-02-12 23:20:25 +0300

    Правка функции составление массива

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@
 // Generate random number
 const getRandomNumber = function (numberMin, numberMax) {
   return numberMin + Math.random() * (numberMax - numberMin);
-}
+};
 
 // Generate random whole number
 const getRandomWholeNumber = function (min, max) {
   if(min >= max || min < 0) {
     throw new Error('getRandomWholeNumber — Неправильный диапазон! Укажите любые числа от 0, чтобы первое было меньше второго.');
   }
-  return Math.floor(getRandomNumber(min, max));
-}
+  return Math.round(getRandomNumber(min, max));
+};
 
 // Generate random number with floating point
 const getRandomFractionalNumber = function (min, max, symbolsAfterPoint) {
@@ -26,22 +26,28 @@ const getRandomFractionalNumber = function (min, max, symbolsAfterPoint) {
     randomNumber = randomNumber.substring(0, randomNumber.length - 1) + getRandomWholeNumber(1, 9) + randomNumber.substring(1 + randomNumber.length - 1);
   }
   return +randomNumber;
-}
+};
 
-const TYPES = [
+const HOUSE_TYPES = [
   'palace',
   'flat',
   'house',
   'bungalow',
 ];
 
-const TIMES = [
+const CHECKIN_TIMES = [
   '12:00',
   '13:00',
   '14:00',
 ];
 
-const FEATURES = [
+const CHECKOUT_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const ROOM_FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -50,59 +56,54 @@ const FEATURES = [
   'conditioner',
 ];
 
-const DESCRIPTIONS = [
+const HOUSE_DESCRIPTIONS = [
   'What is Lorem Ipsum?',
   'Why do we use it?',
   'Where can I get some?',
   'Where does it come from?',
 ];
 
-const IMAGES = [
+const HOUSE_IMAGES = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
-const featuresUsed = [];
-for (let i = 0; i <= getRandomWholeNumber(0, FEATURES.length); i++) {
-  featuresUsed[i] = FEATURES[i];
-}
-
-const photosUsed = [];
-for (let i = 0; i <= getRandomWholeNumber(0, IMAGES.length); i++) {
-  photosUsed[i] = IMAGES[i];
-}
-
-const createDataKeksobooking = () => {
-  let author = {
-    avatar: `img/avatars/user0${getRandomWholeNumber(1, 8)}.png`,
-  };
-  let offer = {
-    title: DESCRIPTIONS[getRandomWholeNumber(0, DESCRIPTIONS.length - 1)],
-    address: `${getRandomFractionalNumber(35.65, 35.7, 5)}, ${getRandomFractionalNumber(139.7, 139.8, 5)}`,
-    price: getRandomWholeNumber(1000, 10000),
-    type: TYPES[getRandomWholeNumber(0, TYPES.length - 1)],
-    rooms: getRandomWholeNumber(1, 10),
-    guests: getRandomWholeNumber(1, 30),
-    checkin: TIMES[getRandomWholeNumber(0, TIMES.length - 1)],
-    checkout: TIMES[getRandomWholeNumber(0, TIMES.length - 1)],
-    features: featuresUsed,
-    description: DESCRIPTIONS[getRandomWholeNumber(0, DESCRIPTIONS.length - 1)],
-    photos: photosUsed,
-  };
-
-  let location = {
-    x: getRandomFractionalNumber(35.65, 35.7, 5),
-    y: getRandomFractionalNumber(139.7, 139.8, 5),
-  };
-
-  return {
-    author,
-    offer,
-    location,
-  }
+// get array of random length
+const getArrayRandomLength = (array) => {
+  return array.slice(getRandomWholeNumber(0, array.length - 1));
 };
 
-const createArrayKeksobooking = new Array(10).fill(null).map(() => createDataKeksobooking());
+// get random element from array
+const getRandomArrayElement = (element) => {
+  return element[getRandomWholeNumber(0, element.length - 1)];
+};
 
-createArrayKeksobooking;
+const createOffersArray = () => {
+  return {
+    author: {
+      avatar: `img/avatars/user0${getRandomWholeNumber(1, 8)}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(HOUSE_DESCRIPTIONS),
+      address: `${getRandomFractionalNumber(35.65, 35.7, 5)}, ${getRandomFractionalNumber(139.7, 139.8, 5)}`,
+      price: getRandomWholeNumber(1000, 10000),
+      type: getRandomArrayElement(HOUSE_TYPES),
+      rooms: getRandomWholeNumber(1, 10),
+      guests: getRandomWholeNumber(1, 30),
+      checkin: getRandomArrayElement(CHECKIN_TIMES),
+      checkout: getRandomArrayElement(CHECKOUT_TIMES),
+      features: getArrayRandomLength(ROOM_FEATURES),
+      description: getRandomArrayElement(HOUSE_DESCRIPTIONS),
+      photos: getArrayRandomLength(HOUSE_IMAGES),
+    },
+    location: {
+      x: getRandomFractionalNumber(35.65, 35.7, 5),
+      y: getRandomFractionalNumber(139.7, 139.8, 5),
+    },
+  };
+};
+
+const offersArray = new Array(10).fill(null).map(() => createOffersArray());
+
+offersArray;
